219e59a7433d3b95d339d2698c1228c098fbfc21
CraveFood Dev <cravefood@user.local>
2026-06-22 01:07:20 +0800

    cleanup: remove root php files causing vercel download issue

--- add_cancel_reason.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('db.php');
-$sql = "ALTER TABLE orders ADD COLUMN CancelReason TEXT DEFAULT NULL;";
-if(mysqli_query($conn, $sql)){
-    echo "Column CancelReason added successfully.";
-} else {
-    echo "Error: " . mysqli_error($conn);
-}
-?>
