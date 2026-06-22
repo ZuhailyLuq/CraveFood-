1dad58b964655172561643127ecd614dc54b4b48
CraveFood Dev <cravefood@user.local>
2026-06-22 12:39:32 +0800

    fix: set PDO::ATTR_EMULATE_PREPARES to true for Supabase Serverless

--- api/db.php
+++ api/db.php
@@ -54,10 +54,10 @@ try {
         $db_user,
         $db_password,
         [
-            PDO::ATTR_PERSISTENT         => true,
+            PDO::ATTR_PERSISTENT         => false,
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-            PDO::ATTR_EMULATE_PREPARES   => false,
+            PDO::ATTR_EMULATE_PREPARES   => true,
         ]
     );
 } catch (PDOException $e) {
